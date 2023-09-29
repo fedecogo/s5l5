d9e3d21206331c4370bd25f2eab07bcfc9a80c9d
fedecogo <141748020+fedecogo@users.noreply.github.com>
2023-09-29 16:01:21 +0200

    versione finita senza extra ora vado a pranzare

--- base_di_partenza/js.js
+++ base_di_partenza/js.js
@@ -4,8 +4,9 @@ let divbianco = document.getElementsByClassName('tred_card')[0]
 let button = document.getElementsByTagName('button')[0];
 let rect = divbianco.getBoundingClientRect()
 let sbianca = rect.top > 0 && rect.bottom <= this.window.innerHeight;
+let navpuntopiùalto = window.scrollY === 0;
 
-if (sbianca){
+if (sbianca || navpuntopiùalto ){
     navbar.style.backgroundColor = '#ffc017 ';
     button.style.backgroundColor = 'black';
 } else {
